a82ecc4007bb753a23f8c0f918c77da04b695c2d
Takorras <sy.empress0321@gmail.com>
2017-09-20 10:50:31 +0900

    [modify]ツイートのセルの下部操作レイアウトを作成

--- app/src/main/kotlin/info/kotlin/kotako/cider/view/TimelineFragment.kt
+++ app/src/main/kotlin/info/kotlin/kotako/cider/view/TimelineFragment.kt
@@ -19,6 +19,7 @@ class TimelineFragment: Fragment(){
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_timeline, container, false)
+        val view = inflater.inflate(R.layout.fragment_timeline, container, false)
+        return view
     }
 }
