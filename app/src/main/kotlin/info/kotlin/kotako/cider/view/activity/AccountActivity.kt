d97aa10aef980a5011b648e85e4f5600ec32870c
Takorras <sy.empress0321@gmail.com>
2017-09-23 17:22:30 +0900

    [add]ログインボタンの実装

--- app/src/main/kotlin/info/kotlin/kotako/cider/view/activity/AccountActivity.kt
+++ app/src/main/kotlin/info/kotlin/kotako/cider/view/activity/AccountActivity.kt
@@ -3,8 +3,14 @@ package info.kotlin.kotako.cider.view.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import com.twitter.sdk.android.core.Callback
+import com.twitter.sdk.android.core.Result
+import com.twitter.sdk.android.core.TwitterException
+import com.twitter.sdk.android.core.TwitterSession
+import com.twitter.sdk.android.core.identity.TwitterLoginButton
 import info.kotlin.kotako.cider.R
 
 class AccountActivity : AppCompatActivity() {
@@ -22,6 +28,25 @@ class AccountActivity : AppCompatActivity() {
     fun setUpView() {
         supportActionBar?.title = "Account"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val loginButton = findViewById(R.id.button_login_with_twitter) as TwitterLoginButton
+        loginButton.callback = ( object : Callback<TwitterSession>() {
+            override fun success(result: Result<TwitterSession>?) {
+                finish()
+                Snackbar.make(findViewById(R.id.layout_account_list), "Successfully", Snackbar.LENGTH_SHORT)
+                        .show()
+            }
+
+            override fun failure(exception: TwitterException?) {
+                Snackbar.make(findViewById(R.id.layout_account_list), "Try again...", Snackbar.LENGTH_SHORT)
+                        .show()
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        (findViewById(R.id.button_login_with_twitter) as TwitterLoginButton).onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
