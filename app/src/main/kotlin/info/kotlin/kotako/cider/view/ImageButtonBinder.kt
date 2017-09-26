511491ba482bdd9639150339c8d425d4c989b26f
Takorras <sy.empress0321@gmail.com>
2017-09-26 22:06:18 +0900

    [modify]アイコンを丸くした

--- app/src/main/kotlin/info/kotlin/kotako/cider/view/ImageButtonBinder.kt
+++ app/src/main/kotlin/info/kotlin/kotako/cider/view/ImageButtonBinder.kt
@@ -3,8 +3,14 @@ package info.kotlin.kotako.cider.view
 import android.databinding.BindingAdapter
 import android.widget.ImageButton
 import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("loadImage")
 fun ImageButton.loadImage(url: String) {
-    Glide.with(context).load(url).into(this)
+    GlideBuilder()
+    Glide.with(context)
+            .load(url)
+            .apply(RequestOptions().circleCrop())
+            .into(this)
 }
